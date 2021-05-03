0f3f7d348084ef7eb9535f4cfd6cd7f54484839d
Michaelpriesthood <priesthood300@gmail.com>
2021-05-03 10:50:58 -0700

    Redesign the activity_todo_list.xml layout

--- app/src/main/java/com/techmedia/todolistapp/TodoListActivity.kt
+++ app/src/main/java/com/techmedia/todolistapp/TodoListActivity.kt
@@ -3,15 +3,16 @@ package com.techmedia.todolistapp
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.techmedia.todolistapp.adapter.TodoAdapter
 import com.techmedia.todolistapp.databinding.ActivityTodoListBinding
 
 class TodoListActivity : AppCompatActivity() {
-    lateinit var binding: ActivityTodoListBinding
+    private lateinit var binding: ActivityTodoListBinding
+
     /**
-     * Create a variable with a type of TodoAdapter that i created for the recyclerview
+     * A variable with a type of TodoAdapter created for the recyclerview
      */
     private lateinit var todoAdapter: TodoAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,30 +20,37 @@ class TodoListActivity : AppCompatActivity() {
         binding = ActivityTodoListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
+        todoAdapter = TodoAdapter(mutableListOf())
         /**
          * Assign the todoAdapter to the todoRecyclerview
          */
         binding.todoRecyclerView.adapter = todoAdapter
+        /**
+         * Handles how the list will be displayed on screen
+         */
+        binding.todoRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        /**
+         * For the toolbar menu
+         */
+        setSupportActionBar(binding.toolbar)
 
         /**
          * Add a click event on the button to navigate to the next activity.
          */
         binding.fabAdd.setOnClickListener {
-            Intent(this, AddTodoActivity::class.java).also { startActivity(it) }
+            Intent(this, AddTodoActivity::class.java).also {
+                startActivity(it)
+            }
         }
-        /**
-         * For the
-         */
-        setSupportActionBar(binding.toolbar)
+
+        val display = intent.getStringExtra("todo_KEY")
     }
 
-
+    /**
+     *   Inflate the menu; this adds items to the action bar if it is present.
+     */
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        /**
-         *   Inflate the menu; this adds items to the action bar if it is present.
-         */
         menuInflater.inflate(R.menu.main_menu, menu)
         return true
     }
--- app/src/main/java/com/techmedia/todolistapp/model/Todo.kt
+++ app/src/main/java/com/techmedia/todolistapp/model/Todo.kt
@@ -1,6 +1,6 @@
 package com.techmedia.todolistapp.model
 
 data class Todo(
-    val title:String,
+    var title:String,
     var isChecked: Boolean = false
 )
